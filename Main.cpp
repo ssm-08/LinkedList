4fa758c74cac279c3aefa232270e72233fb64a37
ssm-08 <shreesainarayanm@gmail.com>
2025-12-11 11:46:43 -0800

    added main

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+#include "Node.h"
+#include "Student.h"
+
+using namespace std;
+
+void add(Student* s);
+void print(Node* n);
+
+int main() {
+
+  Student* one = new Student("Bob", 9);
+  Student* two = new Student("Billy", 10);
+
+  
+
+}
